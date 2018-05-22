27ef3af4943ce9772e474ce16ec58f0906104089
Aleksander Konnerup <Alek.k@hotmail.com>
2018-05-22 14:57:34 +0200

    - Tilføjet Databaseopæstning for TradeBroker - Lavet Simpelt opstart på InitiateTrade-metode og GetTradeData (for test)

--- TradeBroker/TradeBrokerAPI/Controllers/TradeBrokerController.cs
+++ TradeBroker/TradeBrokerAPI/Controllers/TradeBrokerController.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace TradeBrokerAPI.Controllers
 {
@@ -10,9 +14,36 @@ namespace TradeBrokerAPI.Controllers
     public class TradeBrokerController : Controller
     {
         [HttpPost]
-        public void InitiateTrade([FromBody]string stockName, Guid requester)
+        public void InitiateTrade(string stockId, int sharesAmount, [FromBody]string requester)
         {
-            //Do something
+
+            //Modtag request fra StockShareRequester -> InitiateTrade kaldes fra StockShareRequesteren
+            //Request AvailableShares for stockName fra StockShareProvider
+            //If(requestedShares <= AvaiableShares) notify ShareOwnerControl
+            //Modtag svar fra ShareOwnerControl
+            //If(trade == succesful)
+            var options = new DbContextOptionsBuilder<TradeLogContext>()
+                .UseInMemoryDatabase(databaseName: "TradeLogDb")
+                .Options;
+            using (TradeLogContext context = new TradeLogContext(options))
+            {
+                context.Add(new TradeDataModel { StockId = stockId, SharesAmount = sharesAmount, StockRequester = new Guid(requester), StockProvider = new Guid()});
+                context.SaveChanges();
+            }
+        }
+
+        [HttpGet]
+        public TradeDataModel GetTradeData(int tradeDataId)
+        {
+            var options = new DbContextOptionsBuilder<TradeLogContext>()
+                .UseInMemoryDatabase(databaseName: "TradeLogDb")
+                .Options;
+            using (TradeLogContext context = new TradeLogContext(options))
+            {
+                TradeDataModel model;
+                model = context.Find<TradeDataModel>(tradeDataId);
+                return model;
+            }
         }
     }
 }
--- TradeBroker/TradeBrokerAPI/Startup.cs
+++ TradeBroker/TradeBrokerAPI/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Swashbuckle.AspNetCore.Swagger;
+using Microsoft.EntityFrameworkCore;
 
 namespace TradeBrokerAPI
 {
@@ -18,6 +19,8 @@ namespace TradeBrokerAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TradeLogContext>(opt => opt.UseInMemoryDatabase("TradeLogDb"));
+          
             services.AddMvc();
 
             services.AddSwaggerGen(c =>
