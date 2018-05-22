8959d7306cc8dc80652ee76a9c373f77be56ae39
Aleksander Konnerup <Alek.k@hotmail.com>
2018-05-22 14:57:55 +0200

    rest

---
+++ TradeBroker/TradeBrokerAPI/TradeDataModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TradeBrokerAPI
+{
+    public class TradeDataModel
+    {
+        public TradeDataModel()
+        {
+        }
+
+        public int Id { get; set; }
+        public string StockId { get; set; }
+        public int SharesAmount { get; set; }
+        public Guid StockProvider { get; set; }
+        public Guid StockRequester { get; set; }
+    }
+}
---
+++ TradeBroker/TradeBrokerAPI/TradeLogContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TradeBrokerAPI
+{
+    public class TradeLogContext : DbContext
+    {
+        public TradeLogContext(DbContextOptions<TradeLogContext> options) : base(options) { }
+        public DbSet<TradeDataModel> TradeData { get; set; }
+    }
+}
