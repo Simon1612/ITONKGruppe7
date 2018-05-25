1d9b797d5ad3009a1d7ba66fe48ed11010480db7
CSJ <casperstenner@gmail.com>
2018-05-25 21:50:30 +0200

    Added CreateShareDialog. Overall design modifications

--- TradeClient/TradeClient/ViewModels/MainWindowViewModel.cs
+++ TradeClient/TradeClient/ViewModels/MainWindowViewModel.cs
@@ -14,9 +14,6 @@ namespace TradeClient.ViewModels
         public ObservableCollection<Share> AvailableShares { get; set; }
         public ObservableCollection<Share> MyShares { get; set; }
 
-        public ObservableCollection<User> Users { get; set; }
-
-
         public Share SelectedAvailableShare { get; set; }
         public Share SelectedMyShare { get; set; }
         public User CurrentUser { get; set; }
@@ -27,19 +24,19 @@ namespace TradeClient.ViewModels
         public ICommand RefreshCommand { get; set; }
 
         /* Admin */
-
         public ICommand CreateSharesCommand { get; set; }
         public ICommand CreateUserCommand { get; set; }
         public ICommand GenerateUserIdCommand { get; set; }
         public Guid CreateUserGuid { get; set; } = Guid.NewGuid();
 
 
+        /* Common */
+        public ObservableCollection<User> Users { get; set; }
+
 
         public MainWindowViewModel()
         {
-            //StockId, SharePrice
-
-            // Get shares by user ID
+            // Get stuff from db
             AvailableShares = new ObservableCollection<Share>(){ new Share(){Amount = 1337, StockId = "ASDF", Price = 15.32m}};
             MyShares = new ObservableCollection<Share>(){ new Share() { Amount = 1337, StockId = "ASDF", Price = 20.5m }};
             Users = new ObservableCollection<User>();
@@ -53,47 +50,16 @@ namespace TradeClient.ViewModels
             GenerateUserIdCommand = new RelayCommand(OnGenerateUserId);
         }
 
-        private void OnCreateUser()
-        {
-
-            //var user = new User(){UserId = CreateUserGuid};
-
-            var exists = Users.Any(user => user.UserId == CreateUserGuid);
-
-            if (!exists)
-            {
-                //Create user with CreateUserGuid API call
-                Users.Add(new User(){UserId = CreateUserGuid});
-                Notify("Users");
-            }
-        }
-
-        private void OnGenerateUserId()
-        {
-            CreateUserGuid = Guid.NewGuid();
-            Notify("CreateUserGuid");
-        }
-
-        private void OnRefresh()
-        {
-            //Get shares and stuff for current user
-            //Notify collections changed
-        }
-
-        private void OnCreateShares()
-        {
-            throw new NotImplementedException();
-        }
-
+        #region Trading
 
         private void OnMarkSharesForSale()
         {
-            var dlg = new PickAmountDialog(SelectedMyShare) {Owner = App.Current.MainWindow};
+            var dlg = new PickAmountDialog(SelectedMyShare) { Owner = App.Current.MainWindow };
             if (dlg.ShowDialog() == true)
             {
                 var amountToMark = Convert.ToInt32(dlg.AmountTbx.Text);
                 //Mark for sale
-                //Notify collection?
+                //Add and Notify collection or refresh?
             }
         }
 
@@ -105,9 +71,73 @@ namespace TradeClient.ViewModels
             {
                 var amountToMark = Convert.ToInt32(dlg.AmountTbx.Text);
                 //Request buy
-                //Notify collection?
+                //Add and Notify collection or refresh?        
+            }
+
+        }
+
+        private void OnRefresh()
+        {
+            //Get shares and stuff for current user
+            //Notify collections changed
+        }
+
+
+        #endregion
+
+
+        #region Admin
+
+        private void OnCreateUser()
+        {
+
+            //var user = new User(){UserId = CreateUserGuid};
+
+            var userExists = Users.Any(user => user.UserId == CreateUserGuid);
+
+            if (!userExists)
+            {
+                //Create user with CreateUserGuid API call
+                Users.Add(new User() { UserId = CreateUserGuid });
+                Notify("Users");
             }
         }
 
-    }
+        private void OnCreateShares()
+        {
+            var dlg = new CreateShareDialog(Users) { Owner = App.Current.MainWindow };
+            if (dlg.ShowDialog() == true)
+            {
+                var newShare = new Share()
+                {
+                    StockId = dlg.StockIdTbx.Text,
+                    Price = Convert.ToDecimal(dlg.PriceTbx.Text),
+                    Amount = Convert.ToInt32(dlg.AmountTbx.Text)
+                };
+
+                var selectedUser = dlg.SelectUserCbx.SelectionBoxItem;
+
+                //If no owner selected
+                if (String.IsNullOrWhiteSpace(selectedUser.ToString()))
+                {
+                    //Create Share with no owner?
+                }
+                else
+                {
+                    var owner = selectedUser as User;
+                    //Create share with owner
+                }
+            }
+        }
+
+        private void OnGenerateUserId()
+        {
+            CreateUserGuid = Guid.NewGuid();
+            Notify("CreateUserGuid");
+        }
+
+
+        #endregion
+
+
 }
